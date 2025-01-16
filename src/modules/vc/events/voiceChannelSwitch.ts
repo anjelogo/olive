6cc921ba6befac44cd53b8d61aec2b456a429824
Anjelo Go <deorcstudios@gmail.com>
2025-01-15 19:36:22 -0800

    Add logging for voice channel joins and handle permissions for private channels

--- src/modules/vc/events/voiceChannelSwitch.ts
+++ src/modules/vc/events/voiceChannelSwitch.ts
@@ -3,6 +3,7 @@ import { Member, StageChannel, Uncached, VoiceChannel } from "oceanic.js";
 import { create, remove } from "../internals/handler";
 import ExtendedClient from "../../../Base/Client";
 import { moduleData } from "../main";
+import Logging from "../../logging/main";
 
 export const run = async (bot: ExtendedClient, member: Member, channel: Uncached | VoiceChannel | StageChannel, oldChannel: null | VoiceChannel | StageChannel | Uncached): Promise<void> => {
 	if (channel.id === null || channel instanceof StageChannel) return;
@@ -19,4 +20,29 @@ export const run = async (bot: ExtendedClient, member: Member, channel: Uncached
     // solution: use the real member voiceState channelID
     const realChannel = bot.getChannel(member.voiceState?.channelID as string) as VoiceChannel;
     if (realChannel.id === cat.channelID) await create(bot, member, realChannel);
-};
+
+    const channelObj = cat.channels.find((c: Channel) => c.channelID === realChannel.id);
+  
+    if (channelObj) {
+      if (await bot.getModule("Main").handlePermission(member, "vc.join")) {
+        //get logging module
+        const logging = await bot.getModule("Logging") as Logging;
+        logging.log(realChannel.guild, "vc", {embeds: [{
+          type: "rich",
+          title: `${member.username}`,
+          description: `Joined \`${realChannel.name}\``,
+          author: {
+            name: "Joined Private Voice Channel",
+            iconURL: member.avatarURL()
+          },
+          color: bot.constants.config.colors.green,
+          timestamp: new Date().toISOString(),
+          footer: {
+            text: `ID: ${member.id}`
+          }
+        }]});
+      }
+      else 
+        member.edit({ channelID: null });
+    }
+  };
