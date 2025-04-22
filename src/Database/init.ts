5063090fa52157844cb2fb4238423d8f35196347
Anjelo Go <deorcstudios@gmail.com>
2025-04-22 00:34:11 -0700

    fix: update database connection to use environment variable instead of Auth module

--- src/Database/init.ts
+++ src/Database/init.ts
@@ -1,9 +1,8 @@
 import { connect } from "mongoose";
-import { Auth } from "../resources/auth";
 
 const init = async (): Promise<void> => {
 	//load the database
-	await connect(Auth.database);
+	await connect(process.env.DATABASE!);
     
     
 };
