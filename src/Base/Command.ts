797f4812bc10b7d80d8698ab56c6e228cad33430
Anjelo Go <deorcstudios@gmail.com>
2025-04-26 02:27:08 -0700

    fix: standardize indentation in .eslintrc.js, Client.ts, and Command.ts for improved readability

--- src/Base/Command.ts
+++ src/Base/Command.ts
@@ -4,44 +4,44 @@ import { Constants as CustomConstants } from "../resources/interfaces";
 import { FollowupMessageInteractionResponse, InitialMessagedInteractionResponse } from "oceanic.js/dist/lib/util/interactions/MessageInteractionResponse";
 
 export type Options =  ApplicationCommandOptions & {
-	permissions?: string[];
-	requirePerms?: string[];
-	options?: Options[];
+  permissions?: string[];
+  requirePerms?: string[];
+  options?: Options[];
 }
 
 export default class Command {
 
-	public type: (ApplicationCommandTypes | undefined);
-	public disabled = false;
-	public commands: string[];
-	public description: string;
-	public example: (string | null);
-	public enabled: boolean;
-	public devOnly: boolean;
-	public category: string;
-	public permissions?: string[];
-	public requirePerms?: keyof typeof Constants.Permissions;
-	public options?: Options[];
-	public bot: ExtendedClient;
-	public constants: CustomConstants;
-	public guildSpecific?: string[];
-	public execute: (interaction: CommandInteraction) => Promise<InitialMessagedInteractionResponse<CommandInteraction | ComponentInteraction> | FollowupMessageInteractionResponse<CommandInteraction | ComponentInteraction> | InteractionCallbackResponse<AnyInteractionChannel | Uncached> | undefined | void> | undefined;
-	public update: (component: ComponentInteraction) => Promise<Message | undefined | void> | undefined;
-	public modalSubmit: (modal: ModalSubmitInteraction) => Promise<Message | undefined | void> | undefined;
+  public type: (ApplicationCommandTypes | undefined);
+  public disabled = false;
+  public commands: string[];
+  public description: string;
+  public example: (string | null);
+  public enabled: boolean;
+  public devOnly: boolean;
+  public category: string;
+  public permissions?: string[];
+  public requirePerms?: keyof typeof Constants.Permissions;
+  public options?: Options[];
+  public bot: ExtendedClient;
+  public constants: CustomConstants;
+  public guildSpecific?: string[];
+  public execute: (interaction: CommandInteraction) => Promise<InitialMessagedInteractionResponse<CommandInteraction | ComponentInteraction> | FollowupMessageInteractionResponse<CommandInteraction | ComponentInteraction> | InteractionCallbackResponse<AnyInteractionChannel | Uncached> | undefined | void> | undefined;
+  public update: (component: ComponentInteraction) => Promise<Message | undefined | void> | undefined;
+  public modalSubmit: (modal: ModalSubmitInteraction) => Promise<Message | undefined | void> | undefined;
 
-	constructor(bot: ExtendedClient) {
-		this.commands = [];
-		this.description = "No Description Available :(";
-		this.example = "No Example Available :(";
-		this.enabled = true;
-		this.devOnly = false;
-		this.bot = bot;
-		this.constants = bot.constants;
-		this.category = "Uncategorized";
+  constructor(bot: ExtendedClient) {
+    this.commands = [];
+    this.description = "No Description Available :(";
+    this.example = "No Example Available :(";
+    this.enabled = true;
+    this.devOnly = false;
+    this.bot = bot;
+    this.constants = bot.constants;
+    this.category = "Uncategorized";
 
-		this.execute = () => { return undefined; };
-		this.update = () => { return undefined; };
-		this.modalSubmit = () => { return undefined; };
-	}
+    this.execute = () => { return undefined; };
+    this.update = () => { return undefined; };
+    this.modalSubmit = () => { return undefined; };
+  }
 
 }
--- src/Base/Module.ts
+++ src/Base/Module.ts
@@ -6,97 +6,97 @@ import ExtendedClient from "./Client";
 import Command from "./Command";
 
 export interface moduleDataStructure {
-	version: string;
-	guildID: string;
+  version: string;
+  guildID: string;
 }
 
 export default class Module {
 
-	readonly name: string;
-	readonly version: string;
-	readonly bot: ExtendedClient;
-	readonly constants: Constants;
-	readonly path: string;
-	readonly weight: number;
-	readonly db?: boolean;
-	readonly moduleData: unknown;
+  readonly name: string;
+  readonly version: string;
+  readonly bot: ExtendedClient;
+  readonly constants: Constants;
+  readonly path: string;
+  readonly weight: number;
+  readonly db?: boolean;
+  readonly moduleData: unknown;
 
-	constructor (bot: ExtendedClient) {
+  constructor (bot: ExtendedClient) {
 
-		this.name = "";
-		this.version = "0.0";
-		this.bot = bot;
-		this.constants = bot.constants;
-		this.path = "";
-		this.weight = 2; //Loads AFTER Main module | Should go as follows: Main Module => Dependencies => Modules => Sub Modules
-		this.db = false;
+    this.name = "";
+    this.version = "0.0";
+    this.bot = bot;
+    this.constants = bot.constants;
+    this.path = "";
+    this.weight = 2; //Loads AFTER Main module | Should go as follows: Main Module => Dependencies => Modules => Sub Modules
+    this.db = false;
 
-	}
+  }
 
-	readonly data = async (guildID: string): Promise<unknown> => {
-		if (!this.db || !guildID) return;
+  readonly data = async (guildID: string): Promise<unknown> => {
+    if (!this.db || !guildID) return;
 
-		const guild = this.bot.findGuild(guildID);
+    const guild = this.bot.findGuild(guildID);
 
-		if (!guild) return;
+    if (!guild) return;
 
-		let data = await this.bot.db.get(this.name).findOne({ guildID: guild.id });
-		if (!data) {
-			// eslint-disable-next-line @typescript-eslint/no-explicit-any
-			(this.moduleData as any).guildID = guild.id;
+    let data = await this.bot.db.get(this.name).findOne({ guildID: guild.id });
+    if (!data) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (this.moduleData as any).guildID = guild.id;
 
-			this.bot.constants.utils.log(this.name, `Module data not found for guild "${guild.name}" (${guild.id}). Creating now...`);
-			await this.bot.db.get(this.name).bulkWrite([{ updateOne: { filter: { guildID: guild.id }, update: { $set: this.moduleData }, upsert: true }}]);
-			this.bot.constants.utils.log(this.name, `Module data created for guild "${guild.name}".`);
-		}
+      this.bot.constants.utils.log(this.name, `Module data not found for guild "${guild.name}" (${guild.id}). Creating now...`);
+      await this.bot.db.get(this.name).bulkWrite([{ updateOne: { filter: { guildID: guild.id }, update: { $set: this.moduleData }, upsert: true }}]);
+      this.bot.constants.utils.log(this.name, `Module data created for guild "${guild.name}".`);
+    }
 
-		data = await this.bot.db.get(this.name).findOne({ guildID: guild.id });
+    data = await this.bot.db.get(this.name).findOne({ guildID: guild.id });
 
-		return data;
-	}
+    return data;
+  }
 
-	public async load(): Promise<void> {
+  public async load(): Promise<void> {
 
-		this.constants.utils.log(this.name, "Loading...");
+    this.constants.utils.log(this.name, "Loading...");
 
-		//Load Commands
-		const Categories = await fs.readdir(`./${this.path}/commands`, { withFileTypes: true });
+    //Load Commands
+    const Categories = await fs.readdir(`./${this.path}/commands`, { withFileTypes: true });
 
-		for (const Cat of Categories) {
+    for (const Cat of Categories) {
 
-			if (!Cat.isDirectory()) return;
+      if (!Cat.isDirectory()) return;
 
-			const Category: string = Cat.name,
-				Commands = await fs.readdir(`./${this.path}/commands/${Category}`);
+      const Category: string = Cat.name,
+        Commands = await fs.readdir(`./${this.path}/commands/${Category}`);
 
-			Commands.forEach(Command => {
+      Commands.forEach(Command => {
 
-				const cObj: Command = new (require(`../${this.path}/commands/${Category}/${Command}`).default)(this.bot);
-				cObj.category = Category;
+        const cObj: Command = new (require(`../${this.path}/commands/${Category}/${Command}`).default)(this.bot);
+        cObj.category = Category;
 
-				this.bot.commands.push(cObj);
+        this.bot.commands.push(cObj);
 
-			});
-		}
+      });
+    }
 
-		//Load Perms
-		const permissions = await require(`../${this.path}/permnodes`).default;
-		permissions.forEach((permission: Permnodes) => {
-			this.bot.perms.push(permission);
-		});
-		
-		//Load Events
-		const Events = await fs.readdir(`./${this.path}/events`, { withFileTypes: true }); 
-		for (const e of Events) {
-			const event: string = e.name.replace(/\..*/g, "");
+    //Load Perms
+    const permissions = await require(`../${this.path}/permnodes`).default;
+    permissions.forEach((permission: Permnodes) => {
+      this.bot.perms.push(permission);
+    });
+    
+    //Load Events
+    const Events = await fs.readdir(`./${this.path}/events`, { withFileTypes: true }); 
+    for (const e of Events) {
+      const event: string = e.name.replace(/\..*/g, "");
 
-			this.bot.events.find(e => e.name === event)
-				? this.bot.events.find(e => e.name === event).functions.push(require(`../${this.path}/events/${e.name}`))
-				: this.bot.events.push({ name: event, functions: [require(`../${this.path}/events/${e.name}`)] });
-		}
+      this.bot.events.find(e => e.name === event)
+        ? this.bot.events.find(e => e.name === event).functions.push(require(`../${this.path}/events/${e.name}`))
+        : this.bot.events.push({ name: event, functions: [require(`../${this.path}/events/${e.name}`)] });
+    }
 
-		this.constants.utils.log(this.name, "Loaded.");
+    this.constants.utils.log(this.name, "Loaded.");
 
-	}
+  }
 
 }
