306a00cc4bdf523085870c3a91f703ea4e6f67e5
Anjelo Go <deorcstudios@gmail.com>
2025-05-02 15:25:32 -0700

    fix: update logging components to use Constants and improve message formatting for member join/leave events

--- src/modules/logging/events/guildMemberAdd.ts
+++ src/modules/logging/events/guildMemberAdd.ts
@@ -1,4 +1,4 @@
-import { ComponentTypes, Member } from "oceanic.js";
+import { Constants, Member } from "oceanic.js";
 import ExtendedClient  from "../../../Base/Client";
 import Logging from "../main";
 
@@ -8,15 +8,26 @@ export const run = async (bot: ExtendedClient, member: Member): Promise<void> =>
 
   logging.log(member.guild, "welcome", [
     {
-      type: ComponentTypes.CONTAINER,
+      type: Constants.ComponentTypes.CONTAINER,
       accentColor: bot.constants.config.colors.green,
       components: [
         {
-          type: ComponentTypes.TEXT_DISPLAY,
-          content: `# Joined the Server\n\n### ${member.user.username} joined the server!`,
+          type: Constants.ComponentTypes.TEXT_DISPLAY,
+          content: "## Joined the Server",
         }, {
-          type: ComponentTypes.TEXT_DISPLAY,
-          content: `-# Joined at: ${new Date().toLocaleString("en-US")} | User ID: ${member.user.id}`,
+          type: Constants.ComponentTypes.SEPARATOR,
+          spacing: Constants.SeparatorSpacingSize.SMALL,
+          divider: false
+        }, {
+          type: Constants.ComponentTypes.TEXT_DISPLAY,
+          content: `<@${member.user.id}> has joined the server!`,
+        }, {
+          type: Constants.ComponentTypes.SEPARATOR,
+          divider: true,
+          spacing: Constants.SeparatorSpacingSize.LARGE
+        },{
+          type: Constants.ComponentTypes.TEXT_DISPLAY,
+          content: `${bot.constants.emojis.user} <t:${Math.floor(Date.now() / 1000)}:f>`,
         }
       ]
     }
--- src/modules/logging/events/guildMemberRemove.ts
+++ src/modules/logging/events/guildMemberRemove.ts
@@ -1,4 +1,4 @@
-import { ComponentTypes, Guild, Member, Uncached, User } from "oceanic.js";
+import { Constants, Guild, Member, Uncached, User } from "oceanic.js";
 import ExtendedClient from "../../../Base/Client";
 import Logging from "../main";
 
@@ -10,15 +10,26 @@ export const run = async (bot: ExtendedClient, UncachedMember: Member | User, Un
 
   logging.log(bot.findGuild(UncachedGuild.id) as Guild, "welcome", [
     {
-      type: ComponentTypes.CONTAINER,
-      accentColor: bot.constants.config.colors.green,
+      type: Constants.ComponentTypes.CONTAINER,
+      accentColor: bot.constants.config.colors.red,
       components: [
         {
-          type: ComponentTypes.TEXT_DISPLAY,
-          content: `# Joined the Server\n\n### ${user.username} joined the server!`,
+          type: Constants.ComponentTypes.TEXT_DISPLAY,
+          content: "## Left the Server",
         }, {
-          type: ComponentTypes.TEXT_DISPLAY,
-          content: `-# Joined at: ${new Date().toLocaleString("en-US")} | User ID: ${user.id}`,
+          type: Constants.ComponentTypes.SEPARATOR,
+          spacing: Constants.SeparatorSpacingSize.SMALL,
+          divider: false
+        }, {
+          type: Constants.ComponentTypes.TEXT_DISPLAY,
+          content: `<@${user.id}> has left the server!`,
+        }, {
+          type: Constants.ComponentTypes.SEPARATOR,
+          divider: true,
+          spacing: Constants.SeparatorSpacingSize.LARGE
+        },{
+          type: Constants.ComponentTypes.TEXT_DISPLAY,
+          content: `${bot.constants.emojis.user} <t:${Math.floor(Date.now() / 1000)}:f>`,
         }
       ]
     }
