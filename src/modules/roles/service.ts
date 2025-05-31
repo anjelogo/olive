bfa3239093ad2988290de77de37554e541edd28c
Anjelo Go <deorcstudios@gmail.com>
2025-05-30 19:00:10 -0700

    feat(roles): add route handlers and updateData method to RoleService

--- src/modules/roles/service.ts
+++ src/modules/roles/service.ts
@@ -1,12 +1,30 @@
 import { Router } from "express";
 import ExtendedClient from "../../Base/Client";
-import Service from "../../Base/Service";
+import Service, { InputField } from "../../Base/Service";
 
 export default class RoleService extends Service {
 
+  routeHandlers: Record<string, (req: any, res: any) => void> = {
+    "/": (req, res) => {
+      res.send("Role Service is working!");
+    }
+  };
+
+  updateData = (params: any, data: any): Promise<any> =>{
+    // Implement data update logic here
+    return new Promise((resolve, reject) => {
+      try {
+        console.log("Updating data with params:", params, "and data:", data);
+        resolve({ success: true });
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+
   protected bot: ExtendedClient;
   protected router = Router();
-  protected fields = [
+  protected fields: InputField[] = [
     {
       label: "Role Save on Leave",
       description: "Save the role when the user leaves the server",
