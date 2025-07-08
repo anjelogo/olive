bb05d481cfc31dbe72ad34741f4a563878397df6
Anjelo Go <deorcstudios@gmail.com>
2025-07-08 00:16:22 -0700

    fix(autorole): enhance feedback messages with emoji indicators for clarity

--- src/modules/roles/commands/roles/autorole.ts
+++ src/modules/roles/commands/roles/autorole.ts
@@ -88,29 +88,32 @@ export default class Autorole extends Command {
       const subcommand = interaction.data.options.getSubCommand(true)[1];
 
       if (!subcommand)
-        return interaction.createFollowup({content: "Subcommand not found", flags: Constants.MessageFlags.EPHEMERAL});
+        return interaction.createFollowup({content: `${this.bot.constants.emojis.x} Subcommand not found`, flags: Constants.MessageFlags.EPHEMERAL});
       switch(subcommand) {
       case "add": {
         const role = interaction.data.options.getRole("role", true);
 
         if (data.autoRoles.includes(role.id))
-          return interaction.createFollowup({content: "That role is already an Auto Role."});
+          return interaction.createFollowup({content: `${this.bot.constants.emojis.x} That role is already an Auto Role.`});
     
         if (!role)
-          return interaction.createFollowup({content: "I could not find that role"});
+          return interaction.createFollowup({content: `${this.bot.constants.emojis.x} I could not find that role.`});
     
         if (role.position > memberHighestRole.position && !member.permissions.has("ADMINISTRATOR"))
-          return interaction.createFollowup({content: `That role's position is higher than your highest role, ${memberHighestRole.mention}. Perhaps try moving your role higher to solve this problem.`});
+          return interaction.createFollowup({content: `${this.bot.constants.emojis.x} That role's position is higher than your highest role, ${memberHighestRole.mention}. Perhaps try moving your role higher to solve this problem.`});
     
         if (role.position > botHighestRole.position)
-          return interaction.createFollowup({content: `That role's position is higher than my highest role, ${botHighestRole.mention}. Perhaps try moving my role higher to solve this problem.`});
+          return interaction.createFollowup({content: `${this.bot.constants.emojis.x} That role's position is higher than my highest role, ${botHighestRole.mention}. Perhaps try moving my role higher to solve this problem.`});
     
+        if (role.tags.premiumSubscriber || role.tags.guildConnections)
+          return interaction.createFollowup({content: `${this.bot.constants.emojis.x} That role is a premium subscriber or has guild connections, which are not allowed as auto roles.`});
+
         try {
           data.autoRoles.push(role.id);
           await this.bot.updateModuleData("Roles", data, guild);
           return interaction.createFollowup({content: `${this.bot.constants.emojis.tick} Added role ${role.mention} to the roles list!`});
         } catch (e) {
-          return interaction.createFollowup({content: "Error trying to add role to roles list!"});
+          return interaction.createFollowup({content: `${this.bot.constants.emojis.x} Error trying to add role to roles list!`});
         }
       }
     
@@ -118,14 +121,17 @@ export default class Autorole extends Command {
         const role = interaction.data.options.getRole("role", true);
         
         if (!role)
-          return interaction.createFollowup({content: "I could not find that role"});
+          return interaction.createFollowup({content: `${this.bot.constants.emojis.x} I could not find that role.`});
     
         if (role.position > memberHighestRole.position)
-          return interaction.createFollowup({content: `That role's position is higher than your highest role, ${memberHighestRole.mention}. Perhaps try moving your role higher to solve this problem.`});
+          return interaction.createFollowup({content: `${this.bot.constants.emojis.x} That role's position is higher than your highest role, ${memberHighestRole.mention}. Perhaps try moving your role higher to solve this problem.`});
     
         if (role.position > botHighestRole.position)
-          return interaction.createFollowup({content: `That role's position is higher than my highest role, ${botHighestRole.mention}. Perhaps try moving my role higher to solve this problem.`});
+          return interaction.createFollowup({content: `${this.bot.constants.emojis.x} That role's position is higher than my highest role, ${botHighestRole.mention}. Perhaps try moving my role higher to solve this problem.`});
   
+        if (!data.autoRoles.includes(role.id))
+          return interaction.createFollowup({content: `${this.bot.constants.emojis.x} That role is not in the auto roles list.`});
+
         try {
           const i = data.autoRoles.indexOf(role.id);
           if (i > -1) data.autoRoles.splice(i, 1);
@@ -133,7 +139,7 @@ export default class Autorole extends Command {
           await this.bot.updateModuleData("Roles", data, guild);
           return interaction.createFollowup({content: `${this.bot.constants.emojis.tick} Removed role ${role.mention} from the roles list!`});
         } catch (e) {
-          return interaction.createFollowup({content: "Error trying to add role to roles list!"});
+          return interaction.createFollowup({content: `${this.bot.constants.emojis.x} Error trying to remove role from roles list!`});
         }
       }
 
@@ -141,8 +147,8 @@ export default class Autorole extends Command {
         return interaction.createFollowup(
           {
             content: data.autoRoles.length
-              ? `${this.bot.constants.emojis.tick} Users will recieve the following role(s) upon joining: \n\n${data.autoRoles.map((r) => (`<@&${r}>`)).join("\n")}`
-              : `${this.bot.constants.emojis.x} Users recieve no roles upon joining.`,
+              ? `${this.bot.constants.emojis.tick} Users will receive the following role(s) upon joining: \n\n${data.autoRoles.map((r) => (`<@&${r}>`)).join("\n")}`
+              : `${this.bot.constants.emojis.x} Users receive no roles upon joining.`,
           }
         );
         break;
